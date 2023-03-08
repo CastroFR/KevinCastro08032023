9eed4a4294c46b6558cc576568b284975f5601e2
CastroFR <kevinfrancisco906@gmail.com>
2023-03-08 12:53:25 -0600

    Castro.BucleFor

---
+++ BucleFOR.832023/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+class Program 
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("¡Bienvenido al programa de Kevin Castro! \nIngresa un número entero para la altura del triángulo: ");
+            int altura = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= altura; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                    Console.WriteLine();
+    
+            }
+    } 
+}
